2a2306403c19cda310d5ad7b82de9d81f4ac6b9b
gzlj3 <gzlj3@163.com>
2019-01-24 22:38:09 +0800

    home

--- plugin/components/list/list.js
+++ plugin/components/list/list.js
@@ -7,30 +7,26 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    tabItems: Array,
-    activeIndex: {
-      type: Number,
-      value: 0
-    }
-
+    listItems: Array,
+    fields:Object,
   },
 
   data: {
-    list:[]
+    emptyAvatarUrl: '../../images/avatar-empty.png',
   },
   attached: function(){
     // 可以在这里发起网络请求获取插件的数据
-    this.setData({
-      list: [{
-        name: '电视',
-        price: 1000
-      }, {
-        name: '电脑',
-        price: 4000
-      }, {
-        name: '手机',
-        price: 3000
-      }]
-    })
+    // this.setData({
+    //   list: [{
+    //     name: '电视',
+    //     price: 1000
+    //   }, {
+    //     name: '电脑',
+    //     price: 4000
+    //   }, {
+    //     name: '手机',
+    //     price: 3000
+    //   }]
+    // })
   },
 })
